3b06f64a2c9901e5fdf1d90f44384267295335cc
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2023-09-11 09:45:32 -0700

    01_intro_functions_and_scope starter

--- 01_intro_and_functions/index.js
+++ 01_intro_and_functions/index.js
@@ -1,1 +1,53 @@
+// ✅ console.log
 
+// ✅ run js file
+
+// ✅ declare variables with let and const
+
+// ✅ JS 8 data types
+// Primitive values = immutable values lowest level of the language
+// 1. String
+// 2. Number
+// 3. Bigint (+9,000 trillion: good for high precision time stamps, cryptography, financial calculations)
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+
+// muttable (talk more about tomorrow)
+// 8. Object
+
+// ✅ control flow: if else if else
+
+// functions = set of statements to perform a task
+
+// ✅ define a function called sayHello that will print "Hello world!"
+
+// ✅ have that function return "Hello east-se-091123"
+
+// parameters = placeholder, variable, name in function parentheses
+
+// arguments = real value passed to the function
+
+// ✅ add a parameter cohortName
+
+// ✅ change the return value so it says hello to the cohort (string interpolation or template literals)
+
+// Global Scope - variables declared in the global execution context
+// Function Scope - variables and functions inside function body
+// - can access variables and functions defined in global scope
+// - in global scope cannot access function scoped variables and functions
+// Block Scope - Variables declared inside a { } block
+// - cannot be accessed from outside the block
+
+// ✅ indentify variables we have defined in global scope, function scope, block scope
+
+// function declaration and function expression
+
+// first-class functions - first-class objects, means they can be treated like any other object
+//  - assigned to a variable, passed as values to other functions, returned as the value from another function, etc
+//  Higher-order function - function that can take in a function as an argument and or return a function
+//  callback function - function passed as an argument to another function and the other function will invoke it
+
+// ✅ define a function called emileysSchedule that takes in a time and will return true if that time (hour as a number) is between 10 and 11 otherwise will return false
+// ✅ define a higher-order function called areYouAvailable that takes in another function as an argument as well as a time (hour as a number) and returns whether the person is available at that time
