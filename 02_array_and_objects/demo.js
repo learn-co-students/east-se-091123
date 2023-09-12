1460cf614e95d54e8b9fb66bbcbaab14768fbea9
Adam La Rosa <adam.larosa@flatironschool.com>
2023-09-12 05:03:39 -0700

    array & object starter

---
+++ 02_array_and_objects/demo.js
@@ -0,0 +1,32 @@
+// ✅ 1. Adding to the end of an array -> .push
+
+// ✅ 2. Adding to the beginning of an array -> .unshift
+
+// ✅ 3.  Remove from the end of an array -> .pop
+
+// ✅ 4.  Remove from the beginning of an array -> .shift
+
+// ✅ 5.  Remove a number of elements from an array -> .slice
+
+// ✅ 6.  Modify an array by adding elements -> .splice
+
+// ✅ 7.  Creating a copy of an array -> the spread operator, i.e. "..."
+
+
+// ✅ 8.  Adding keys to an object -> dot & bracket notation
+
+// ✅ 9.  Deleting keys from an object -> "delete" keyword
+
+// ✅ 10. Creating a copy of an object -> the spread operator, i.e. "..."
+
+// ✅ 11. Iterating through an object -> "for in" loop
+
+
+// ✅ 12. Finding something in an array -> .find
+
+// ✅ 13. Filtering out an array -> .filter
+
+// ✅ 14. Creating a new array from another array -> .map
+
+// ✅ 15. Doing some work for every element in an array -> .forEach
+
