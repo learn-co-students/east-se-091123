051dfeea32052c50b4d650dee5c7886f0d15942f
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2023-09-27 11:50:48 -0700

    DONE: added algorithms folder and reverse string algorithm

---
+++ algorithms/reverse-string.js
@@ -0,0 +1,18 @@
+// LEETCODE LINK => https://leetcode.com/problems/reverse-string/
+
+// 1. Spend time understanding the problem 
+//     - put the problem into your own words
+//     - ASK QUESTIONS!
+
+
+// 2. Write own test cases
+
+
+// 3. Pseudocode 
+
+
+// 4. Code!
+
+
+// 5. Refactor (make code readable, variables make sense)
+// 6. Optimize
