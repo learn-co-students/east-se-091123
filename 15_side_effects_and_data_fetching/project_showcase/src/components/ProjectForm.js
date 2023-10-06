3f19450eb40c2030a7d0dd2e1a773a0b4f9d5a0d
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2023-10-06 08:23:47 -0700

    DONE: update starter code with last lecture

--- 15_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
+++ 15_side_effects_and_data_fetching/project_showcase/src/components/ProjectForm.js
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ProjectForm() {
+const initialState = {
+  name: "",
+  about: "",
+  phase: "",
+  link: "",
+  image: "",
+};
+
+function ProjectForm({ onNewProject }) {
+  const [formData, setFormData] = useState(initialState);
+  const { name, phase, about, link, image } = formData;
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFormData((currentStateFormData) => {
+      return {
+        ...currentStateFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const project = {
+      ...formData,
+      id: Math.floor(Math.random() * 100),
+    };
+    // // make post request to server
+    onNewProject(project);
+
+    setFormData(initialState);
+  }
+
   return (
     <section>
-      <form className="form" autoComplete="off">
+      <form className="form" autoComplete="off" onSubmit={handleSubmit}>
         <h3>Add New Project</h3>
 
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
 
         <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+        <textarea
+          id="about"
+          name="about"
+          value={about}
+          onChange={handleChange}
+        />
 
         <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
+        <select name="phase" id="phase" value={phase} onChange={handleChange}>
           <option>Select One</option>
           <option value="1">Phase 1</option>
           <option value="2">Phase 2</option>
@@ -23,10 +69,22 @@ function ProjectForm() {
         </select>
 
         <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+        <input
+          type="text"
+          id="link"
+          name="link"
+          value={link}
+          onChange={handleChange}
+        />
 
         <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+        <input
+          type="text"
+          id="image"
+          name="image"
+          value={image}
+          onChange={handleChange}
+        />
 
         <button type="submit">Add Project</button>
       </form>
