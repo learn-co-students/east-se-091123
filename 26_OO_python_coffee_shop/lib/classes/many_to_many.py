92bd748b3bcd8ddd6469d2499bca0f18d2a92b4f
Emiley Palmquist <emiley.palmquist@gmail.com>
2023-10-30 12:14:49 -0700

    DONE: review solution coffe shop mock

--- 26_OO_python_coffee_shop/lib/classes/many_to_many.py
+++ 26_OO_python_coffee_shop/lib/classes/many_to_many.py
@@ -1,34 +1,90 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not hasattr(self, "_name"):
+            if type(new_name) == str and len(new_name) >= 3:
+                self._name = new_name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list({order.customer for order in self.orders()})
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        if self.num_orders() == 0: 
+            return 0
+        sum_prices = sum([order.price for order in self.orders()])
+        # sum_prices = 0
+        # for order in self.orders():
+        #     sum_prices += order.price
+        return sum_prices / self.num_orders()
 
 class Customer:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if type(new_name) == str and 1 <= len(new_name) <= 15:
+            self._name = new_name
+
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        return list({order.coffee for order in self.orders()})
     
     def create_order(self, coffee, price):
-        pass
+        return Order(self, coffee, price)
     
 class Order:
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if not hasattr(self, "_price"):
+            if isinstance(new_price, float) and 1.0 <= new_price <= 10.0:
+                self._price = new_price
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @customer.setter
+    def customer(self, new_customer):
+        if isinstance(new_customer, Customer):
+            self._customer = new_customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, new_coffee):
+        if type(new_coffee) == Coffee:
+            self._coffee = new_coffee
--- 26_OO_python_coffee_shop/lib/debug.py
+++ 26_OO_python_coffee_shop/lib/debug.py
@@ -8,4 +8,13 @@ from classes.many_to_many import Coffee
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
+    c1 = Customer('Emiley')
+    c2 = Customer('Kailey')
+    coffee1 = Coffee('Mocha')
+    coffee2 = Coffee('Flat white')
+    coffee3 = Coffee('Latte')
+    o1 = Order(c1, coffee1, 8.0)
+    o2 = Order(c1, coffee1, 5.0)
+    o3 = Order(c1, coffee1, 2.0)
+    
     ipdb.set_trace()
