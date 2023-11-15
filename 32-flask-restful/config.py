78d876022f23e4b556cbca69a8f924e8f0a5fbbb
Emiley Palmquist <emiley.palmquist@gmail.com>
2023-11-15 09:49:53 -0800

    DONE: flask-restful solution

--- 32-flask-restful/config.py
+++ 32-flask-restful/config.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api
 
 app = Flask( __name__ )
 
@@ -13,3 +14,4 @@ db = SQLAlchemy()
 migrate = Migrate( app, db )
 
 db.init_app( app )
+api = Api(app)
--- 32-flask-restful/models/cat.py
+++ 32-flask-restful/models/cat.py
@@ -1,14 +1,17 @@
-
+from sqlalchemy_serializer import SerializerMixin
 from config import db
 
-class Cat( db.Model ):
+class Cat( db.Model, SerializerMixin ):
     __tablename__ = 'cats'
+    serialize_rules = ('-updated_at', '-created_at',)
 
     id = db.Column( db.Integer, primary_key = True )
     name = db.Column( db.String )
     age = db.Column( db.Integer )
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    def as_dict( self ):
-        # this returns a dictionary version of the instance
-        return { 'id': self.id, 'name': self.name, 'age': self.age }
+    # def as_dict( self ):
+    #     # this returns a dictionary version of the instance
+    #     return { 'id': self.id, 'name': self.name, 'age': self.age }
 
