d6f85d1179b7387fe64bdcb401bc6bc11a29f631
Emiley Palmquist <emiley.palmquist@gmail.com>
2023-12-11 11:44:50 -0800

    DONE: review formik errors and validations

--- 37-auth/client/src/components/Signup.js
+++ 37-auth/client/src/components/Signup.js
@@ -7,18 +7,24 @@ function Signup({ setUser }) {
     const [signup, setSignup] = useState(true)
 
     const signupSchema = yup.object().shape({
-        username: yup.string().min(5, 'Too Short!').max(15, 'Too Long!').required('Required!'),
+        username: yup.string().min(5, 'Username too Short!').max(15, 'Username too Long!'),
         email: yup.string().email('Invalid email'),
-        password: yup.string().min(5, 'Too Short!').max(15, 'Too Long!').required('Required!')
+        password: yup.string().min(5, 'Password too Short!').max(15, 'Password too Long!'),
+        passwordConfirmation: yup.string().required('Confirm Password').oneOf([yup.ref('password')], 'Passwords must match')
+    })
+    const loginSchema = yup.object().shape({
+        username: yup.string().required('username required'),
+        password: yup.string().required('password required')
     })
 
     const formik = useFormik({
         initialValues: {
             username: '',
             email: '',
-            password: ''
+            password: '',
+            passwordConfirmation: ''
         },
-        validationSchema: signupSchema,
+        validationSchema: signup ? signupSchema : loginSchema,
         onSubmit: (values) => {
             const endpoint = signup ? '/users' : '/login'
             fetch(endpoint, {
@@ -45,37 +51,56 @@ function Signup({ setUser }) {
         setSignup((currentSignup) => !currentSignup)
     }
 
+    console.log(formik.errors)
+
     return (
         <Box>
-            {/* {formik.errors} */}
+            {/* { Object.keys(formik.errors).map((key) => <li>{formik.errors[key]}</li>) } */}
             <Button onClick={toggleSignup}>{signup ? 'Login instead!' : 'Register for an account'}</Button>
             <form onSubmit={formik.handleSubmit}>
                 <TextField 
                     id="username" 
                     label="Username" 
                     variant="outlined" 
+                    error={!!formik.errors.username}
                     required
                     value={formik.values.username}
                     onChange={formik.handleChange}
                 />
-            
+                {formik.errors.username && <li>{formik.errors.username}</li>}
                 {signup && <TextField 
                     id="email"
                     label="email"
                     variant="outlined" 
+                    error={!!formik.errors.email}
                     required
                     value={formik.values.email}
                     onChange={formik.handleChange}
                 />}
+                 {formik.errors.email && <li>{formik.errors.email}</li>}
                 <TextField 
                     id="password"
                     label="password"
-                    type="password"
+                    type="text"
                     variant="outlined" 
+                    error={!!formik.errors.password}
                     required
                     value={formik.values.password}
                     onChange={formik.handleChange}
                 />
+                 {formik.errors.password && <li>{formik.errors.password}</li>}
+                {signup && <TextField 
+                    id="passwordConfirmation"
+                    label="passwordConfirmation"
+                    type="text"
+                    variant="outlined" 
+                    error={!!formik.errors.passwordConfirmation}
+                    required
+                    value={formik.values.passwordConfirmation}
+                    onChange={formik.handleChange}
+                />}
+                { formik.errors.passwordConfirmation && <li>{formik.errors.passwordConfirmation}</li>}
+                
                 <Button variant="contained" type="submit">Submit</Button>
             </form>
         </Box>
